491e3041a5fae442f7d994f96f1cabfb4fcb36b8
zuri <webzuri@gmail.com>
2019-11-26 01:35:54 +0100

    MPoolTests, add MPoolReserve in test

--- src/base/MPoolTests.c
+++ src/base/MPoolTests.c
@@ -3,7 +3,9 @@
 
 #include <zrlib/base/MemoryPool/MemoryPool.h>
 #include <zrlib/base/MemoryPool/MPoolDynamicStrategy.h>
+#include <zrlib/base/MemoryPool/MPoolReserve.h>
 #include <zrlib/base/Allocator/CAllocator.h>
+#include <zrlib/base/macro.h>
 #include "../main.h"
 
 // ============================================================================
@@ -12,17 +14,42 @@ ZRAllocator *ALLOCATOR;
 
 // ============================================================================
 
+size_t const testGet_nb = 15;
+
+ZRMemoryPool* testGet_createDynamic(void)
+{
+	return ZRMPoolDS_createBS(1, sizeof(double), ALLOCATOR);
+}
+
+ZRMemoryPool* testGet_createReserve()
+{
+	return ZRMPoolReserve_create(sizeof(double), testGet_nb, ALLOCATOR);
+}
+
 MU_TEST(testGet)
 {
-	size_t const nb = 15;
-	ZRMemoryPool *pool = ZRMPoolDS_createBS(1, sizeof(double), ALLOCATOR);
-	double *val = ZRMPool_reserve_nb(pool, nb);
+	size_t const nb = testGet_nb;
 
-	for (int i = 0; i < nb; i++)
-		val[i] = i;
+	struct
+	{
+		ZRMemoryPool* (*create)(void);
+		void (*destroy)(ZRMemoryPool*);
+	} pools[] = { //
+		{ testGet_createDynamic, ZRMPoolDS_destroy }, //
+		{ testGet_createReserve, ZRMPoolReserve_destroy }, //
+		};
 
-	ZRMPool_release_nb(pool, &val[1], 2);
-	ZRMPoolDS_destroy(pool);
+	for (size_t i = 0; i < ZRCARRAY_NBOBJ(pools); i++)
+	{
+		ZRMemoryPool *pool = pools[i].create();
+		double *val = ZRMPool_reserve_nb(pool, nb);
+
+		for (int i = 0; i < nb; i++)
+			val[i] = i;
+
+		ZRMPool_release_nb(pool, &val[1], 2);
+		pools[i].destroy(pool);
+	}
 }
 
 // ============================================================================
