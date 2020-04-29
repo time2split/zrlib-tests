4d3a8bd574a7f6e44265dbd1a517a3b415f07d03
zuri <webzuri@gmail.com>
2020-04-29 19:39:39 +0200

    PoolVsMalloc, test all reserve

--- src/benchmark/versus/PoolVsMalloc.c
+++ src/benchmark/versus/PoolVsMalloc.c
@@ -7,55 +7,103 @@
 #include <stdio.h>
 #include <time.h>
 
+#include <zrlib/base/macro.h>
 #include <zrlib/base/Allocator/CAllocator.h>
 #include <zrlib/base/MemoryPool/MPoolDynamicStrategy.h>
+#include <zrlib/base/MemoryPool/MPoolReserve.h>
 
 int PoolVsMalloc(int argc, char **argv)
 {
 	const size_t nb = 100;
 	const size_t allocSize = 16;
 	const size_t maxFreeBuckets = 10;
-	const size_t nbObj = 10000;
-	const size_t initialBSize = 100;
+	const size_t nbObj = 1000;
+	const size_t initialBSize = 10000;
+	const size_t step = 90;
 
 	ZRAllocator *allocator = malloc(sizeof(ZRAllocator));
 	ZRCAllocator_init(allocator);
-	ZRMemoryPool *pool = ZRMPoolDS_create(initialBSize, maxFreeBuckets, allocSize, allocator);
-	char *p[nbObj];
-	clock_t time_naive = 0;
-	clock_t time_intrinsic = 0;
+	struct
 	{
+		char *name;
+		ZRMemoryPool *pool;
+	} pools[] = { //
+		"bits", ZRMPoolReserve_create(allocSize, alignof(max_align_t), initialBSize, allocator, ZRMPoolReserveMode_bits), //
+		"list", ZRMPoolReserve_create(allocSize, alignof(max_align_t), initialBSize, allocator, ZRMPoolReserveMode_list), //
+		"chunk", ZRMPoolReserve_create(allocSize, alignof(max_align_t), initialBSize, allocator, ZRMPoolReserveMode_chunk), //
+		"Dyn:chunk", ZRMPoolDS_create(initialBSize, maxFreeBuckets, allocSize, allocator), //
+		};
+
+	char *p[nbObj];
+
+	{
+		clock_t time = 0;
+		clock_t time_alloc = 0;
+		clock_t time_free = 0;
 		clock_t first = clock();
 		for (size_t i = 0; i < nb; i++)
 		{
-			for(size_t j = 0; j < nbObj; j++)
-			p[j] = malloc(allocSize);
+			clock_t time_a;
 
-			for(size_t j = 0; j < nbObj; j++)
-			free(p[j]);
+			time_a = clock();
+			for (size_t j = 0; j < nbObj; j++)
+				p[j] = malloc(allocSize);
+
+			time_alloc += clock() - time_a;
+
+			time_a = clock();
+			for (size_t j = 0; j < nbObj; j++)
+				free(p[j]);
+
+			time_free += clock() - time_a;
 		}
 		clock_t end = clock();
-		time_naive = end - first;
+		time = end - first;
+		printf("naive:%10lu ; alloc:%10lu free:%10lu\n", time, time_alloc, time_free);
 	}
+
+	for (int pool_i = 0; pool_i < ZRCARRAY_NBOBJ(pools); pool_i++)
 	{
+		clock_t time = 0;
+		clock_t time_alloc = 0;
+		clock_t time_free = 0;
+
+		ZRMemoryPool *pool = pools[pool_i].pool;
+		char *name = pools[pool_i].name;
+
 		clock_t first = clock();
 		for (size_t i = 0; i < nb; i++)
 		{
-			for(size_t j = 0; j < nbObj; j++)
+			clock_t time_a;
+
+			time_a = clock();
+			for (size_t j = 0; j < nbObj; j += step)
+				p[j] = ZRMPool_reserve_nb(pool, step);
+
+			time_alloc += clock() - time_a;
+
+			for (size_t j = 0; j < nbObj; j += step)
 			{
-				p[j] = ZRMPool_reserve(pool);
+				if (p[j] == NULL)
+				{
+					fprintf(stderr, "%s: Can't allocate i=%lu j=%lu\n", name, i, j);
+					exit(1);
+				}
 			}
 
-			for(size_t j = 0; j < nbObj; j++)
-			ZRMPool_release(pool, p[j]);
-//			ZRMPool_release_nb(pool, p[0], nbObj);
+			time_a = clock();
+//			for (size_t j = 0; j < nbObj; j++)
+//				ZRMPool_release_nb(pool, p[j], 1);
+
+			ZRMPool_clean(pool);
+			time_free += clock() - time_a;
 		}
 		clock_t end = clock();
-		time_intrinsic = end - first;
-	}
+		time = end - first;
 
-	printf("naive:%ld ; %LE pool:%ld %LE\n", time_naive, (long double)time_naive / nb, time_intrinsic, (long double)time_intrinsic / nb);
-	ZRMPool_destroy(pool);
+		printf("%5s:%10lu ; alloc:%10lu free:%10lu\n", name, time, time_alloc, time_free);
+		ZRMPool_destroy(pool);
+	}
 	free(allocator);
 	return 0;
 }
