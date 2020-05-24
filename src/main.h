64f1461ac03a28cad4647a398042811099336f21
zuri <webzuri@gmail.com>
2020-05-24 21:42:29 +0200

    main.h, add ZRTEST_ASSERT_TYPE_OP()

--- src/main.h
+++ src/main.h
@@ -32,15 +32,35 @@ do{ \
 
 #define ZRTEST_CHECK(COND) \
 do{ \
-	if(!(COND)) mu_fail(FUN_PREFIX); \
+	if(!(COND)) { \
+		strcat(FUN_PREFIX, " (" #COND ")\n"); \
+		mu_fail(FUN_PREFIX); \
+	} \
 }while(0)
+
+#define ZRTEST_ASSERT_TYPE_OP(E,R,TYPE,OP,PRINT) \
+do{ \
+	TYPE _e = (E); \
+	TYPE _r = (R); \
+	\
+	if(!(_e OP _r)) { \
+		sprintf(FUN_PREFIX, "%sExpected " #OP PRINT " but have " PRINT "\n", FUN_PREFIX, _e, _r); \
+		mu_fail(FUN_PREFIX); \
+	} \
+}while(0)
+
+#define ZRTEST_ASSERT_INT_EQ(E,R) ZRTEST_ASSERT_TYPE_OP(E,R,int,==,"%d")
+#define ZRTEST_ASSERT_INT_NEQ(E,R) ZRTEST_ASSERT_TYPE_OP(E,R,int,!=,"%d")
+
+#define ZRTEST_ASSERT_PTR_EQ(E,R) ZRTEST_ASSERT_TYPE_OP(E,R,void*,==,"%p")
+#define ZRTEST_ASSERT_PTR_NEQ(E,R) ZRTEST_ASSERT_TYPE_OP(E,R,void*,!=,"%p")
 
 #define ZRTEST_END(msg_buffer, result, expected) \
 	ZRTEST_RESULT(FUN_PREFIX, msg_buffer, result, expected)
 
 // ============================================================================
 
-static void testResult(const char *prefix, char* MESSAGE_BUFF, void *result, void* expected, int (*cmpResult)(void*, void*), void (*print)(char*, void*))
+static void testResult(const char *prefix, char *MESSAGE_BUFF, void *result, void *expected, int (*cmpResult)(void*, void*), void (*print)(char*, void*))
 {
 	const int cmp = cmpResult(result, expected);
 
@@ -54,7 +74,7 @@ static void testResult(const char *prefix, char* MESSAGE_BUFF, void *result, voi
 
 		sprintf(MESSAGE_BUFF, "%s (cmp=%d) Expected\n%s but have\n%s\n", prefix, cmp, msg_expected, msg_have);
 		fputs(MESSAGE_BUFF, stderr);
-		fflush(stderr);
+		fflush (stderr);
 		mu_fail(MESSAGE_BUFF);
 	}
 }
