5fcb8739c1e7a13fa43b868e28b0456dc16c8b84
zuri <webzuri@gmail.com>
2020-10-24 11:34:15 +0200

    VectorTests, add CONFIGS

--- src/base/Vector/VectorTests.c
+++ src/base/Vector/VectorTests.c
@@ -1,18 +1,65 @@
 #include <minunit/minunit.h>
 #include <stdint.h>
 
+#include <zrlib/base/AddressOp.h>
 #include <zrlib/base/Vector/Vector.h>
 #include <zrlib/base/Allocator/CAllocator.h>
 #include <zrlib/base/Vector/Vector2SideStrategy.h>
 #include "../../main.h"
 
-ZRAllocator *ALLOCATOR;
+static ZRAllocator *ALLOCATOR;
+static ZRInitInfos_t INITINFOS;
+
+#define CONFIG_XLIST() \
+	X(fixedVector, true) \
+	X(dynamicVector, false)
+
+#define ZRVECTORTEST_BEGIN() ZRTEST_PRINTF("(%s) ", CONFIG.name)
 
 #define LOCAL_ZRTEST_RESULT(prefix) \
 	ZRTEST_RESULT(prefix, MESSAGE_BUFF, result, expected)
 
 #define LOCAL_ZRTEST_END() \
 	ZRTEST_END(MESSAGE_BUFF, result, expected)
+
+typedef struct
+{
+	char *name;
+	ZRVector* (*fcreate)(ZRObjInfos objInfos, size_t initialArraySize, size_t initialMemorySize);
+	unsigned fixed :1;
+} Config;
+
+ZRVector* create_fixedVector(ZRObjInfos objInfos, size_t initialArraySize, size_t initialMemorySize)
+{
+	ZRVector2SideStrategyInfos(INITINFOS, objInfos);
+	ZRVector2SideStrategyInfos_allocator(INITINFOS, ALLOCATOR);
+	ZRVector2SideStrategyInfos_initialArraySize(INITINFOS, initialArraySize);
+	ZRVector2SideStrategyInfos_initialMemorySize(INITINFOS, initialMemorySize);
+	ZRVector2SideStrategyInfos_fixedArray(INITINFOS);
+	return ZRVector2SideStrategy_new(INITINFOS);
+}
+
+ZRVector* create_dynamicVector(ZRObjInfos objInfos, size_t initialArraySize, size_t initialMemorySize)
+{
+	ZRVector2SideStrategyInfos(INITINFOS, objInfos);
+	ZRVector2SideStrategyInfos_allocator(INITINFOS, ALLOCATOR);
+	ZRVector2SideStrategyInfos_initialArraySize(INITINFOS, initialArraySize);
+	ZRVector2SideStrategyInfos_initialMemorySize(INITINFOS, initialMemorySize);
+	return ZRVector2SideStrategy_new(INITINFOS);
+}
+
+#define X(NAME, FIXED) # NAME, create_ ## NAME, FIXED,
+static Config CONFIGS[] = { CONFIG_XLIST() };
+#undef X
+static Config CONFIG;
+
+#define ASSERT_FIXED() ZRBLOCK( \
+	if(false == CONFIG.fixed) return; \
+)
+
+#define ASSERT_DYNAMIC() ZRBLOCK( \
+	if(true == CONFIG.fixed) return; \
+)
 
 // ============================================================================
 
@@ -21,72 +68,49 @@ static void testSetup()
 	mainTestSetup();
 }
 
-ZRVector* createFixedVector(size_t initialArraySize, size_t initialMemorySize)
+static void printArray_long(char *out, long *array, size_t nbObj)
 {
-	return ZRVector2SideStrategy_createFixed(initialArraySize, ZRTYPE_SIZE_ALIGNMENT(long), ALLOCATOR);
-}
-
-ZRVector* createLongDynamicVector(size_t initialArraySize, size_t initialMemorySize)
-{
-	return ZRVector2SideStrategy_createDynamicM(initialArraySize, initialMemorySize, ZRTYPE_SIZE_ALIGNMENT(long), ALLOCATOR);
-}
-
-ZRVector* createDynamicVector(size_t initialArraySize, size_t initialMemorySize, size_t objSize, size_t objAlignment)
-{
-	return ZRVector2SideStrategy_createDynamicM(initialArraySize, initialMemorySize, objSize, objAlignment, ALLOCATOR);
-}
-
-void deleteVector(ZRVector *vec)
-{
-	ZRVector_destroy(vec);
-}
-
-// ============================================================================
-
-static int FUN_CMP_NAME(ZRVector *result, ZRVector *expected)
-{
-	int cmp;
-	cmp = ZRVector_nbObj(result) - ZRVector_nbObj(expected);
-
-	if (cmp)
-		return cmp;
-
-	size_t nbObj = ZRVector_nbObj(result);
-	size_t objSize = ZRVector_objSize(result);
-
-	while (nbObj--)
-	{
-		cmp = memcmp(ZRVector_get(result, nbObj), ZRVector_get(expected, nbObj), objSize);
-
-		if (cmp)
-			return cmp;
-	}
-	return 0;
-}
-
-static void FUN_PRINT_NAME(char *out, ZRVector *vec)
-{
-	char buff[2048];
-	size_t nbObj = ZRVector_nbObj(vec);
+	char buff[128];
 	size_t i = 0;
 	strcat(out, "[");
 
 	while (nbObj--)
 	{
-		long *l = ZRVector_get(vec, i);
-		sprintf(buff, "%s%ld", (i ? "," : ""), *l);
+		long l = array[i];
+		snprintf(buff, ZRCARRAY_NBOBJ(buff), "%s%ld", (i ? "," : ""), l);
 		strcat(out, buff);
 		i++;
 	}
 	strcat(out, "]\n");
 }
+
+static bool checkArray_long(long *lexpected, size_t nbObj, ZRVector *result)
+{
+	for (size_t i = 0; i < nbObj; i++)
+	{
+		long res = *(long*)ZRVECTOR_GET(result, i);
+
+		if (res != lexpected[i])
+		{
+			char buffer_e[4096] = "";
+			char buffer_r[4096] = "";
+			printArray_long(buffer_e, lexpected, nbObj);
+			printArray_long(buffer_r, ZRVECTOR_ARRAYP(result), nbObj);
+			ZRTEST_CATPRINTF("failed: expected\n%s but have\n%s\n", buffer_e, buffer_r);
+			return false;
+		}
+	}
+	return true;
+}
+
 // ============================================================================
 
 MU_TEST(testInsert)
 {
-	ZRTEST_BEGIN();
-	ZRVector *result = createFixedVector(8, 0);
-	ZRVector *expected = createFixedVector(8, 0);
+	ZRVECTORTEST_BEGIN();
+	long lexpected[] = { 1000, 1001, 100, 21, 22, 1 };
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 8, 0);
+
 	long i[8];
 	i[0] = 100;
 	ZRVector_add(result, i);
@@ -101,243 +125,262 @@ MU_TEST(testInsert)
 	i[1] = 1001;
 	ZRVector_addFirst_nb(result, 2, i);
 
-	long lexpected[] = { 1000, 1001, 100, 21, 22, 1 };
-	ZRVector_add_nb(expected, sizeof(lexpected) / sizeof(long), lexpected);
-	LOCAL_ZRTEST_END();
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
 
-	deleteVector(result);
-	deleteVector(expected);
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testSize)
 {
-	ZRTEST_BEGIN();
-	ZRVector *result = createFixedVector(8, 0);
+	ZRVECTORTEST_BEGIN();
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 2, 0);
 	long data = 0;
 
-	mu_check(ZRVector_nbObj(result) == 0);
+	ZRTEST_ASSERT_INT_EQ(0, ZRVector_nbObj(result));
 	ZRVector_add(result, &data), data++;
-	mu_check(ZRVector_nbObj(result) == 1);
+	ZRTEST_ASSERT_INT_EQ(1, ZRVector_nbObj(result));
 	ZRVector_add(result, &data), data++;
-	mu_check(ZRVector_nbObj(result) == 2);
+	ZRTEST_ASSERT_INT_EQ(2, ZRVector_nbObj(result));
 
-	deleteVector(result);
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicGrowArrayInitialIs0)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
 	size_t const result_initialMemorySize = 2;
-	ZRVector *result = createLongDynamicVector(0, result_initialMemorySize);
-	ZRVector *expected = createFixedVector(nb, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 0, result_initialMemorySize);
+	long lexpected[nb];
 
 	for (long i = 0; i < nb; i++)
 	{
 		ZRVector_add(result, &i);
-		ZRVector_add(expected, &i);
+		lexpected[i] = i;
 	}
-	LOCAL_ZRTEST_END();
 
-	deleteVector(result);
-	deleteVector(expected);
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicGrowMemoryInitialIs0)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
 	size_t const result_initialArraySize = 10;
-	ZRVector *result = createLongDynamicVector(result_initialArraySize, 0);
-	ZRVector *expected = createFixedVector(nb, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), result_initialArraySize, 0);
+	long lexpected[nb];
 
 	for (long i = 0; i < nb; i++)
 	{
 		ZRVector_add(result, &i);
-		ZRVector_add(expected, &i);
+		lexpected[i] = i;
 	}
-	LOCAL_ZRTEST_END();
 
-	deleteVector(result);
-	deleteVector(expected);
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicGrow)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
 	size_t const result_initialArraySize = 10;
 	size_t const result_initialMemorySize = 20;
-	ZRVector *result = createLongDynamicVector(result_initialArraySize, result_initialMemorySize);
-	ZRVector *expected = createFixedVector(nb, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), result_initialArraySize, result_initialMemorySize);
+	long lexpected[nb];
 
 	for (long i = 0; i < nb; i++)
 	{
 		ZRVector_add(result, &i);
-		ZRVector_add(expected, &i);
+		lexpected[i] = i;
 	}
-	LOCAL_ZRTEST_END();
 
-	deleteVector(result);
-	deleteVector(expected);
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicGrowAddOverflowSizeInitialArray)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
 	size_t const result_initialArraySize = 10;
-	ZRVector *result = createLongDynamicVector(result_initialArraySize, 0);
-	ZRVector *expected = createFixedVector(nb, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), result_initialArraySize, 0);
+	long lexpected[nb];
 
 	for (long i = 0; i < nb; i++)
-	{
-		ZRVector_add(expected, &i);
-	}
-	ZRVector_add_nb(result, nb, ZRVECTOR_PARRAY(expected));
-	LOCAL_ZRTEST_END();
+		lexpected[i] = i;
 
-	deleteVector(result);
-	deleteVector(expected);
+	ZRVector_add_nb(result, nb, lexpected);
+
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicGrowAddOverflowSizeInitialMemory)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
 	size_t const result_initialMemorySize = 10;
-	ZRVector *result = createLongDynamicVector(0, result_initialMemorySize);
-	ZRVector *expected = createFixedVector(nb, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 0, result_initialMemorySize);
+	long lexpected[nb];
 
 	for (long i = 0; i < nb; i++)
-	{
-		ZRVector_add(expected, &i);
-	}
-	ZRVector_add_nb(result, nb, ZRVECTOR_PARRAY(expected));
-	LOCAL_ZRTEST_END();
+		lexpected[i] = i;
 
-	deleteVector(result);
-	deleteVector(expected);
+	ZRVector_add_nb(result, nb, lexpected);
+
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicShrinkLeft)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 15;
 	size_t const less = 8;
 	size_t const offset = 2;
-	ZRVector *result = createLongDynamicVector(0, nb);
-	ZRVector *expected = createFixedVector(nb - less, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 0, nb);
+	long lexpected[nb - less];
 
-	for (long i = 0; i < nb; i++)
+	for (long i = 0, ei = 0; i < nb; i++)
 	{
 		ZRVector_add(result, &i);
 
 		if (i < offset || i >= offset + less)
-			ZRVector_add(expected, &i);
+			lexpected[ei++] = i;
 	}
 	ZRVector_delete_nb(result, offset, less);
-	LOCAL_ZRTEST_END();
 
-	deleteVector(result);
-	deleteVector(expected);
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testDynamicShrinkRight)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 15;
 	size_t const less = 8;
 	size_t const offset = 6;
-	ZRVector *result = createLongDynamicVector(0, nb);
-	ZRVector *expected = createFixedVector(nb - less, 0);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 0, nb);
+	long lexpected[nb - less];
 
-	for (long i = 0; i < nb; i++)
+	for (long i = 0, ei = 0; i < nb; i++)
 	{
 		ZRVector_add(result, &i);
 
 		if (i < offset || i >= offset + less)
-			ZRVector_add(expected, &i);
+			lexpected[ei++] = i;
 	}
 	ZRVector_delete_nb(result, offset, less);
-	LOCAL_ZRTEST_END();
 
-	deleteVector(result);
-	deleteVector(expected);
+	if (false == checkArray_long(lexpected, ZRCARRAY_NBOBJ(lexpected), result))
+		ZRTEST_FAIL();
+
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testTrim)
 {
-	ZRTEST_BEGIN();
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 15;
 	size_t const capacity = nb * 3;
-	ZRVector *result = createLongDynamicVector(0, capacity);
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(long), 0, capacity);
 
 	for (long i = 0; i < nb; i++)
-	{
 		ZRVector_add(result, &i);
-	}
-	mu_check(capacity - nb <= ZRVECTOR_SIZE(result));
-	mu_assert_int_eq(nb, ZRVECTOR_NBOBJ(result));
+
+	ZRTEST_CHECK(capacity - nb <= ZRVECTOR_CAPACITY(result));
+	ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_NBOBJ(result));
 
 	ZRVector_memoryTrim(result);
-	mu_assert_int_eq(nb, ZRVECTOR_SIZE(result));
-	mu_assert_int_eq(nb, ZRVECTOR_NBOBJ(result));
 
-	ZRVector_add(result, (int[] ) { 12 });
-	mu_check(nb <= ZRVECTOR_SIZE(result));
-	mu_assert_int_eq(nb + 1, ZRVECTOR_NBOBJ(result));
+	if (false == CONFIG.fixed)
+		ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_CAPACITY(result));
 
-	deleteVector(result);
+	ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_NBOBJ(result));
+
+	if (false == CONFIG.fixed)
+	{
+		ZRVector_add(result, (int[] ) { 12 });
+		ZRTEST_CHECK(nb <= ZRVECTOR_CAPACITY(result));
+		ZRTEST_ASSERT_INT_EQ(nb + 1, ZRVECTOR_NBOBJ(result));
+	}
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testChangeSize)
 {
-#define T1 char
-#define T2 uint64_t
+	typedef char T1[4];
+	typedef uint64_t T2;
 
-	ZRTEST_BEGIN();
+	size_t const T1_align = 2;
+	ZRVECTORTEST_BEGIN();
 	size_t const nb = 100;
-	size_t const capacity = nb * 3;
-	ZRVector *result = createDynamicVector(capacity, capacity, sizeof(T1), 7);
+	size_t const capacity = (nb + 1) * sizeof(T2);
+	ZRVector *result = CONFIG.fcreate(ZROBJINFOS_DEF(T1_align, sizeof(T1)), capacity, capacity);
 
-	for (T1 i = 0; i < nb; i++)
+	for (T1 i = { 0 }; i[0] < nb; i[0]++)
 		ZRVector_add(result, &i);
 
-	mu_assert_int_eq(nb, ZRVECTOR_NBOBJ(result));
-	ZRVector_changeObjSize(result, ZRTYPE_SIZE_ALIGNMENT(T2));
-	mu_assert_int_eq(0, ZRVECTOR_NBOBJ(result));
+	ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_NBOBJ(result));
+	ZRTEST_CHECK(ZRAddress_aligned_a(ZRVECTOR_ARRAYP(result), T1_align));
+	return;
+	ZRVector_changeObjSize(result, ZRTYPE_OBJINFOS(T2));
+	ZRTEST_ASSERT_INT_EQ(0, ZRVECTOR_NBOBJ(result));
 
 	for (T2 i = 0; i < nb; i++)
 		ZRVector_add(result, &i);
 
-	mu_assert_int_eq(nb, ZRVECTOR_NBOBJ(result));
-	deleteVector(result);
-#undef T1
-#undef T2
+	ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_NBOBJ(result));
+	ZRTEST_ASSERT_PTR_EQ(ZRVECTOR_GET(result, 0), ZRVECTOR_ARRAYP(result));
+	ZRTEST_ASSERT_INT_EQ(0, ZRAddress_aligned_a(ZRVECTOR_ARRAYP(result), alignof(T2)));
+	ZRTEST_ASSERT_INT_EQ(nb / 2, (*(T2* )ZRVECTOR_GET(result, nb / 2)));
+	ZRVector_destroy(result);
 }
 
 MU_TEST(testStress)
 {
-	ZRTEST_BEGIN();
+	ASSERT_DYNAMIC();
+	ZRVECTORTEST_BEGIN();
 #define T1 int
 
 	ZRTEST_BEGIN();
 	size_t const nb = 10000;
 	size_t const nb_rest = 13;
 	size_t const capacity = nb * 3;
-	ZRVector *result = createDynamicVector(0, 1, ZRTYPE_SIZE_ALIGNMENT(T1));
+	ZRVector *result = CONFIG.fcreate(ZRTYPE_OBJINFOS(T1), 0, 1);
 
 	for (T1 i = 0; i < nb; i++)
 		ZRVector_add(result, &i);
 
-	mu_assert_int_eq(nb, ZRVECTOR_NBOBJ(result));
+	ZRTEST_ASSERT_INT_EQ(nb, ZRVECTOR_NBOBJ(result));
 
 	for (T1 i = 0; i < nb - nb_rest; i++)
 		ZRVector_dec(result);
 
-	mu_assert_int_eq(nb_rest, ZRVECTOR_NBOBJ(result));
-	deleteVector(result);
+	ZRTEST_ASSERT_INT_EQ(nb_rest, ZRVECTOR_NBOBJ(result));
+	ZRVector_destroy(result);
 }
 
 // ============================================================================
@@ -367,7 +410,12 @@ int VectorTests(void)
 
 	puts(__FUNCTION__);
 	MU_SUITE_CONFIGURE(testSetup, NULL);
-	MU_RUN_SUITE(AllTests);
+
+	for (size_t i = 0; i < ZRCARRAY_NBOBJ(CONFIGS); i++)
+	{
+		CONFIG = CONFIGS[i];
+		MU_RUN_SUITE(AllTests);
+	}
 	MU_REPORT()
 				;
 	free(ALLOCATOR);
