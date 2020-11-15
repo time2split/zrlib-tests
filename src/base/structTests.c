b82296028de10b2d5fc2e366e2be1f046064f96c
zuri <webzuri@gmail.com>
2020-11-15 01:43:31 +0100

    structTests, add ObjInfos_union

--- src/base/structTests.c
+++ src/base/structTests.c
@@ -1,4 +1,7 @@
 #include <zrlib/base/struct.h>
+
+#include <zrlib/lib/init.h>
+#include <zrlib/base/Allocator/Allocator.h>
 #include "../main.h"
 
 // ============================================================================
@@ -58,6 +61,43 @@ MU_TEST(arithmResize)
 	ZRTEST_ASSERT_INT_EQ(24, infos[2].size);
 }
 
+MU_TEST(ObjInfos_union)
+{
+	ZRTEST_BEGIN();
+	unsigned flags = ZRSTRUCT_FLAG_ARITHMETIC;
+	ZRObjInfos ret, a = ZROBJINFOS_DEF(2, 7),
+		b = ZROBJINFOS_DEF(8, 17),
+		c = ZROBJINFOS_DEF(24, 70),
+		d = ZROBJINFOS_DEF(150, 1)
+			;
+
+	ret = ZRObjInfos_union_flags_l(flags, a, b);
+	ZRTEST_ASSERT_INT_EQ(8, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(17, ret.size);
+
+	ret = ZRObjInfos_union_flags_l(flags, a, b, c);
+	ZRTEST_ASSERT_INT_EQ(24, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(70, ret.size);
+
+	ret = ZRObjInfos_union_flags_l(flags, a, b, d);
+	ZRTEST_ASSERT_INT_EQ(600, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(17, ret.size);
+
+	/* Array version */
+
+	ret = ZROBJINFOS_UNION_FLAGS((ZRObjInfos[] ) { a, b }, 2, flags);
+	ZRTEST_ASSERT_INT_EQ(8, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(17, ret.size);
+
+	ret = ZROBJINFOS_UNION_FLAGS((ZRObjInfos[] ) { a, b, c }, 3, flags);
+	ZRTEST_ASSERT_INT_EQ(24, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(70, ret.size);
+
+	ret = ZROBJINFOS_UNION_FLAGS((ZRObjInfos[] ) { a, b, d }, 3, flags);
+	ZRTEST_ASSERT_INT_EQ(600, ret.alignment);
+	ZRTEST_ASSERT_INT_EQ(17, ret.size);
+}
+
 // ============================================================================
 // TESTS
 // ============================================================================
@@ -66,6 +106,7 @@ MU_TEST_SUITE( AllTests)
 {
 	MU_RUN_TEST(arithm);
 	MU_RUN_TEST(arithmResize);
+	MU_RUN_TEST(ObjInfos_union);
 }
 
 int structTests(void)
